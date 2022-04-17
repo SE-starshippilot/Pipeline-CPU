c734af51a2ee97ca5df6ecb3c24469a89b300e51
tianhao shi <120090472@link.cuhk.edu.cn>
2022-04-16 19:33:03 -0700

    added 5-bit 2-1 mux

--- Mux.v
+++ Mux.v
@@ -5,3 +5,11 @@ module Mux2_1_32BIT(In_A, In_B, SEL, Out);
 
     assign Out = (SEL == 0)? In_A: In_B;
 endmodule
+
+module Mux2_1_5BIT(In_A, In_B, SEL, Out);
+    input[4:0] In_A, In_B;
+    input SEL;
+    output [4:0] Out;
+
+    assign Out = (SEL == 0)? In_A: In_B;
+endmodule

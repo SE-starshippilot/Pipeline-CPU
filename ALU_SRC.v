19ed7b52fec0e1f2dc7bea537c0236dbc5ce5a29
tianhao shi <120090472@link.cuhk.edu.cn>
2022-04-18 09:17:58 -0700

    added more comments to ALU_SRC

--- ALU_SRC.v
+++ ALU_SRC.v
@@ -14,25 +14,25 @@ module ALU_SRC(src_ctrl,
     
     always @(reg_A, reg_B, shamt, imm, src_ctrl) begin
         case(src_ctrl)
-        0: begin
+        0: begin                                        //for other R-type instruction
                 out_A <= reg_A;
                 out_B <= reg_B;
             end
-        1: begin
+        1: begin                                        //for andi, xori, ori
                 out_A <= reg_A;
-                out_B <= {{16{1'b0}}, imm};    //zero-extend immediate to 32-bit     
+                out_B <= {{16{1'b0}}, imm};             //zero-extend immediate to 32-bit     
             end
-        2: begin
+        2: begin                                        //for other types of I-type instruction
                 out_A <= reg_A;
-                out_B <= {{16{imm[15]}}, imm}; //sign-extend immediate to 32-bit
+                out_B <= {{16{imm[15]}}, imm};          //sign-extend immediate to 32-bit
             end
-        3: begin
+        3: begin                                        //for sllv, srlv, srav
                 out_A <= reg_B;
-                out_B <= {{27{1'b0}}, reg_A[4:0]}; //lower 5 bits of register A  
+                out_B <= {{27{1'b0}}, reg_A[4:0]};      //lower 5 bits of register A  
             end
-        4: begin
+        4: begin                                        //for sll, srl, sra
                 out_A <= reg_B;
-                out_B <= {{27{1'b0}}, shamt};  // zero-extended shift amount to 32-bit
+                out_B <= {{27{1'b0}}, shamt};           //zero-extended shift amount to 32-bit
             end
         endcase
     end

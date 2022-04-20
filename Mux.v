2caa52470ad92e246fcd1432aeec96f1e3e5e1da
tianhao shi <120090472@link.cuhk.edu.cn>
2022-04-20 04:45:23 -0700

    deleted repeated MUX by assigning bus width to MUX

--- Mux.v
+++ Mux.v
@@ -1,15 +1,7 @@
-module Mux2_1_32BIT(In_A, In_B, SEL, Out);
-    input[31:0] In_A, In_B;
+module Mux2_1 #(parameter WIDTH = 32)(In_A, In_B, SEL, Out);
+    input[WIDTH - 1:0] In_A, In_B;
     input SEL;
-    output [31:0] Out;
-
-    assign Out = (SEL == 0)? In_A: In_B;
-endmodule
-
-module Mux2_1_5BIT(In_A, In_B, SEL, Out);
-    input[4:0] In_A, In_B;
-    input SEL;
-    output [4:0] Out;
+    output [WIDTH - 1:0] Out;
 
     assign Out = (SEL == 0)? In_A: In_B;
 endmodule

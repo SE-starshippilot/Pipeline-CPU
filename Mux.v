4d328e1f9c386506ff7ff50ab8b2f7471a620c6d
tianhao shi <120090472@link.cuhk.edu.cn>
2022-04-20 22:45:38 -0700

    added 3_1 register and modified output to be reg type

--- Mux.v
+++ Mux.v
@@ -1,7 +1,23 @@
 module Mux2_1 #(parameter WIDTH = 32)(In_A, In_B, SEL, Out);
     input[WIDTH - 1:0] In_A, In_B;
     input SEL;
-    output [WIDTH - 1:0] Out;
+    output reg[WIDTH - 1:0] Out;
+    always @(*) begin
+        Out <= (SEL == 1'b0)? In_A: In_B;
+    end
+endmodule
 
-    assign Out = (SEL == 0)? In_A: In_B;
+module Mux3_1 #(parameter WIDTH = 32)(In_A, In_B, In_C, SEL, Out);
+    input[WIDTH - 1:0] In_A, In_B, In_C;
+    input[1:0] SEL;
+    output reg[WIDTH - 1:0] Out;
+
+    always @(*) begin
+        case(SEL)
+            0:       Out <= In_A;
+            1:       Out <= In_B;
+            2:       Out <= In_C;
+            default: Out <= -1;
+        endcase
+    end
 endmodule

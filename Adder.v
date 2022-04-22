b8a35e17012bf66d682968705461ed2fd6a965bf
tianhao shi <120090472@link.cuhk.edu.cn>
2022-04-22 02:29:56 -0700

    added bandwith

--- Adder.v
+++ Adder.v
@@ -1,6 +1,6 @@
-module ADDER_32BIT(In_A, In_B, Out);
-input [31:0] In_A, In_B;
-output [31:0] Out;
+module ADDER#(parameter WIDTH=32)(In_A, In_B, Out);
+input [WIDTH - 1:0] In_A, In_B;
+output [WIDTH - 1:0] Out;
 
 assign Out = In_A + In_B;
 
